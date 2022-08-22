8c394dd94ff18684d7d643dad3816796d77ed6ea
Rafal <rafalkubala97@gmail.com>
2022-08-22 14:16:34 +0200

    Added start script and server configuration

---
+++ index.js
@@ -0,0 +1,19 @@
+// Tworzenie Servera
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const PORT = 4000;
+
+app.use(express.static(
+  path.join(__dirname, "/")
+));
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
