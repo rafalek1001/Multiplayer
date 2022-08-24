dcfc643c0af515a510c3a92cf3fb8d0f391d29d2
Rafal <rafalkubala97@gmail.com>
2022-08-24 09:55:09 +0200

    Added form, socket connection and ability to join and quit the game

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.use(express.static(
   path.join(__dirname, "/")
 ));
 
+io.on('connection', (socket) => {
+  socket.on('join-game', (data) => {
+    socket.playerName = data.playerName;
+    console.log(`Gracz ${data.playerName} dołączył do gry.`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`Gracz ${socket.playerName} wyszedł z gry.`);
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
 });
